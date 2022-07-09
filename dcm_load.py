f1a2c46df64803d0c4716924509fc5b88a00a678
Agam main <work.agamchopra@gmail.com>
2022-07-09 16:11:26 -0500

    description added

--- dcm_load.py
+++ dcm_load.py
@@ -6,10 +6,24 @@ from PIL import Image
 
 
 def load_dcm_as_np(path = None, plot = False, norm = True):
+    """
+    Parameters
+    ----------
+    path : string, optional
+        The location of the image on disk without the .dcm extension. ex- "C:\image" The default is None.
+    plot : binary, optional
+        Prints the .dcm image. The default is False.
+    norm : binary, optional
+        Does the image need to be normalized before return. The default is True.
 
+    Returns
+    -------
+    data : numpy float array
+        The .dcm image as a numpy 2D array.
+
+    """
     if path != None:
         dcm = pydicom.read_file(path + '.dcm')
-        print(dcm)
             
         data = apply_modality_lut(dcm.pixel_array, dcm)
         
@@ -17,6 +31,7 @@ def load_dcm_as_np(path = None, plot = False, norm = True):
             data = (data - np.min(data))/(np.max(data) - np.min(data))
         
         if plot:
+            print(dcm)
             plt.imshow(data,cmap='gray')
             plt.show()
             print(data.shape)
@@ -27,13 +42,24 @@ def load_dcm_as_np(path = None, plot = False, norm = True):
         return None
     
     
-def save_as_png(x,path):
+def save_as_tif(x,path):
     Image.fromarray(x).save(path + '.tif')
     
     
 def dcm2tif(path = None, plot = False, norm = True):
+    """
+    Parameters
+    ----------
+    path : string, optional
+        The location of the image on disk without the .dcm extension. ex- "C:\image" The default is None.
+    plot : binary, optional
+        Prints the .dcm image. The default is False.
+    norm : binary, optional
+        Does the image need to be normalized before return. The default is True.
+    """
     x = load_dcm_as_np(path, plot, norm)
-    save_as_png(x, path)
+    save_as_tif(x, path)
+    
     
     
 def load_tif(path = None):
@@ -41,10 +67,13 @@ def load_tif(path = None):
     
     
 if __name__ == "__main__":
-    path = input('Enter full file path. (ex- ../xyz) DO NOT ENTER FILE EXTENSION .dcm, etc!\n')
-    dcm2tif(path,True,True)
-    x = load_tif(path)
-    print(x.shape, type(x))
-    print(x)
-    plt.imshow(x,cmap='gray')
-    plt.show()
+    #path = input('Enter full file path. (ex- ../xyz) DO NOT ENTER FILE EXTENSION .dcm, etc!\n')
+    for i in range(4):
+        path = 'R:\Ilka_NU\img (%d)'%(i+1)
+        dcm2tif(path = path,norm = True)
+        x = load_tif(path)
+        
+        print(x.shape, type(x))
+        print(x)
+        plt.imshow(x,cmap='gray')
+        plt.show()
