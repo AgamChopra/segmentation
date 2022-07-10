f3493fe8b3eaf0ebcc5cfe88ea567ac26088ed56
Agam main <work.agamchopra@gmail.com>
2022-07-10 12:10:16 -0500

    descriptions added
    quality of life updates

--- dcm_load.py
+++ dcm_load.py
@@ -1,13 +1,14 @@
 import numpy as np
 import pydicom
 from pydicom.pixel_data_handlers.util import apply_modality_lut
-import matplotlib.pyplot as plt
 from PIL import Image
 from PIL.TiffTags import TAGS
 
+import matplotlib.pyplot as plt
+
 
 def load_dcm_as_np(path = None, plot = False, norm = True):
-    """
+    '''
     Parameters
     ----------
     path : string, optional
@@ -21,13 +22,11 @@ def load_dcm_as_np(path = None, plot = False, norm = True):
     -------
     data : numpy float array
         The .dcm image as a numpy 2D array.
-
-    """
+    '''
     if path != None:
         dcm = pydicom.read_file(path + '.dcm')
         
-        #meta = str(dcm.file_meta)
-        meta = str(dcm)
+        meta = str(dcm) #meta = str(dcm.file_meta)
             
         data = apply_modality_lut(dcm.pixel_array, dcm)
         
@@ -48,11 +47,25 @@ def load_dcm_as_np(path = None, plot = False, norm = True):
     
     
 def save_as_tif(path,x,meta = None):
-    Image.fromarray(x).save(path + '.tif',description=meta)
+    '''
+    Parameters
+    ----------
+    path : string
+        The location of the image on disk without the .dcm extension. ex- "C:\image" The default is None.
+    x : numpy array
+        2x2 numpy array holding the pixel values of the loaded image.
+    meta : string, optional
+        Meta data of the orignal dicom file. The default is None.
+
+    Returns
+    -------
+    None.
+    '''
+    Image.fromarray(x).save(path + '.tif',description = meta)
     
     
 def dcm2tif(path = None, plot = False, norm = True):
-    """
+    '''
     Parameters
     ----------
     path : string, optional
@@ -61,18 +74,33 @@ def dcm2tif(path = None, plot = False, norm = True):
         Prints the .dcm image. The default is False.
     norm : binary, optional
         Does the image need to be normalized before return. The default is True.
-    """
+    '''
     x,meta = load_dcm_as_np(path, plot, norm)
     save_as_tif(path,x,meta)
     
     
     
 def load_tif(path = None):
+    '''
+    Parameters
+    ----------
+    path : string, optional
+        The location of the image on disk without the .dcm extension. ex- "C:\image" The default is None.
+
+    Returns
+    -------
+    data : numpy array
+        2x2 numpy array holding the pixel values of the loaded image.
+    tif_meta : dict
+        The entire meta data of the tif file.
+    orignal_meta_description : string
+        Meta data of the orignal dicom file.
+    '''
     with Image.open(path + '.tif') as img:
         data = np.array(img)
-        meta = {TAGS[key] : img.tag[key] for key in img.tag.keys()}
-        orignal_meta_description = meta['ImageDescription'][0]
-    return data, meta, orignal_meta_description
+        tif_meta = {TAGS[key] : img.tag[key] for key in img.tag.keys()}
+        orignal_meta_description = tif_meta['ImageDescription'][0]
+    return data, tif_meta, orignal_meta_description
     
     
 if __name__ == "__main__":
@@ -80,8 +108,8 @@ if __name__ == "__main__":
     for i in range(4):
         path = 'R:\Ilka_NU\img (%d)'%(i+1)
         dcm2tif(path = path,norm = True)
-        x,_,meta = load_tif(path)
         
+        x,_,meta = load_tif(path)       
         print(x.shape, type(x))
         print(meta)
         plt.imshow(x,cmap='gray')
